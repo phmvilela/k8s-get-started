2240b0cb97180b3bc34921d4e5684f34a0b3dbe6
Pedro Vilela <pedro.vilela@benevolent.ai>
2017-07-26 11:28:47 +0100

    Added integration with mongo and printing hostname in the response to see load balancer in action

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,21 @@
 from flask import Flask, request
+from pymongo import MongoClient
+from socket import gethostname
+import time
 
+mongo = MongoClient('mongodb://mongo:27017/')
+db = mongo.k8sdb
 app = Flask(__name__)
+
+current_time_millis = lambda: int(round(time.time() * 1000))
 
 @app.route("/")
 def main():
-    return '{"message" : "This is a k8s get started test. V5"}', 200, {"Content-Type" : "application/json"}
+    hostname = gethostname()
+    time = current_time_millis()
+    db.datasets.insert_one({"hostname" : hostname, "timestamp" : time})
+    json = '{"message" : "This is a k8s get started app. hostname: %s"}' % gethostname()
+    return json, 200, {"Content-Type" : "application/json"}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=True)
